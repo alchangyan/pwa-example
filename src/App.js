b38a711b6a25aa5696fb5423b7df892a040b38f2
Artem Alchangyan <artem.alchangyan@example.com>
2021-12-31 21:33:02 +0400

    added notofocation functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { useEffect, useCallback } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const delay = 2;
+
 function App() {
+  /**
+   * Possible values for permission
+   * - default
+   * - granted
+   * - denied
+   */
+  const permission = Notification.permission;
+
+  const handleCLick = useCallback(() => {
+    setTimeout(() => {
+      new Notification("Hi there!");
+    }, delay * 1000);
+  }, []);
+
+  useEffect(() => {
+    if (["default", "denied"].includes(permission)) {
+      Notification.requestPermission().then((result) => {
+        console.log(result);
+      });
+    }
+  }, [permission]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>React PWA Example</p>
+        <ul>
+          <li>PUSH API Permission: {permission}</li>
+          <li>
+            Send test notification({delay}s delay):{" "}
+            <button onClick={handleCLick}>Send</button>
+          </li>
+        </ul>
       </header>
     </div>
   );
